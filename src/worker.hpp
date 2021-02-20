bc5da99b208985306a76e941087afdb99a5b83bc
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-02-20 19:20:30 +0900

    add worker file

---
+++ src/worker.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) Database Group, Nagoya University. All rights reserved.
+// Licensed under the MIT license.
+
+#pragma once
+
+class Worker
+{
+ private:
+  /* data */
+ public:
+  Worker(/* args */) {}
+  ~Worker() = default;
+};
