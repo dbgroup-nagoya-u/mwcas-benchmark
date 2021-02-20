4c8fe9e5491fb1ac03bd28e89eaf11d761b83603
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-02-20 21:10:38 +0900

    add fundamental structure

--- src/worker.hpp
+++ src/worker.hpp
@@ -3,11 +3,53 @@
 
 #pragma once
 
+#include <array>
+#include <cstdint>
+#include <vector>
+
 class Worker
 {
  private:
-  /* data */
+  /*################################################################################################
+   * Internal enum and constants
+   *##############################################################################################*/
+
+  static constexpr auto kPrivateFieldNum = 128UL;
+
+  /*################################################################################################
+   * Internal member variables
+   *##############################################################################################*/
+
+  size_t read_ratio_;
+
+  size_t operation_counts_;
+
+  std::vector<size_t> exec_times_nano_;
+
+ protected:
+  /*################################################################################################
+   * Inherited member variables
+   *##############################################################################################*/
+
+  std::array<size_t, kPrivateFieldNum> private_fields_;
+
+  std::vector<size_t*> shared_fields_;
+
  public:
-  Worker(/* args */) {}
+  /*################################################################################################
+   * Public constructors/destructors
+   *##############################################################################################*/
+
+  Worker(  //
+      const std::vector<size_t*> shared_fields,
+      const size_t read_ratio,
+      const size_t operation_counts)
+      : read_ratio_{read_ratio}, operation_counts_{operation_counts}
+  {
+    private_fields_.fill(0);
+    shared_fields_.insert(shared_fields_.begin(), shared_fields.begin(), shared_fields.end());
+    exec_times_nano_.reserve(operation_counts_);
+  }
+
   ~Worker() = default;
 };
