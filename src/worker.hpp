9c45e7c23d5be736be6203073c438b7450864032
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-04-09 12:21:11 +0900

    divide function to measure latency and throughput

--- src/worker.hpp
+++ src/worker.hpp
@@ -21,7 +21,13 @@ class Worker
 
   size_t operation_counts_;
 
-  std::vector<std::pair<OperationType, size_t>> exec_times_nano_;
+  size_t random_seed_;
+
+  std::vector<Operation> operation_queue_;
+
+  std::vector<size_t> exec_times_nano_;
+
+  size_t exec_time_nano_;
 
  protected:
   /*################################################################################################
@@ -46,13 +52,16 @@ class Worker
       const size_t private_field_num,
       size_t *shared_fields,
       const size_t read_ratio,
-      const size_t operation_counts)
+      const size_t operation_counts,
+      const size_t random_seed = 0)
       : read_ratio_{read_ratio},
         operation_counts_{operation_counts},
+        random_seed_{random_seed},
+        exec_time_nano_{0},
         shared_num_{shared_field_num},
         private_num_{private_field_num}
   {
-    exec_times_nano_.reserve(operation_counts_);
+    PrepareBench();
     shared_fields_ = shared_fields;
     for (size_t count = 0; count < private_field_num; ++count) {
       private_fields_[count] = 0;
@@ -65,43 +74,90 @@ class Worker
    * Public utility functions
    *##############################################################################################*/
 
-  virtual size_t
+  virtual void
   ReadMwCASField()
   {
-    return 0;
   }
 
-  virtual size_t
+  virtual void
   PerformMwCAS()
   {
-    return 0;
   }
 
   void
-  Run()
+  PrepareBench()
   {
-    std::random_device seed_gen;
-    std::mt19937_64 rand_engine{seed_gen()};
+    // initialize vectors
+    exec_times_nano_.clear();
+    exec_times_nano_.reserve(operation_counts_);
+    operation_queue_.clear();
+    operation_queue_.reserve(operation_counts_);
 
-    for (size_t count = 0; count < operation_counts_; ++count) {
-      std::this_thread::sleep_for(std::chrono::nanoseconds(100));
-
-      const auto rand_val = rand_engine() % 100;
-      if (rand_val < read_ratio_) {
-        // perform a MwCAS read operation
-        const auto exec_time = ReadMwCASField();
-        exec_times_nano_.emplace_back(OperationType::kRead, exec_time);
-      } else {
-        // perform a MwCAS operation
-        const auto exec_time = PerformMwCAS();
-        exec_times_nano_.emplace_back(OperationType::kWrite, exec_time);
-      }
+    // generate an operation-queue for benchmark
+    std::mt19937_64 rand_engine{random_seed_};
+    for (size_t i = 0; i < operation_counts_; ++i) {
+      const auto ops = (rand_engine() % 100 < read_ratio_) ? Operation::kRead : Operation::kWrite;
+      operation_queue_.emplace_back(ops);
     }
   }
 
-  std::vector<std::pair<OperationType, size_t>>
-  GetResults()
+  void
+  MeasureLatency()
   {
-    return std::move(exec_times_nano_);
+    assert(operation_queue_.size() == operation_counts_);
+    assert(exec_times_nano_.empty());
+
+    for (size_t i = 0; i < operation_counts_; ++i) {
+      const auto start_time = std::chrono::high_resolution_clock::now();
+      switch (operation_queue_[i]) {
+        case kRead:
+          ReadMwCASField();
+          break;
+        case kWrite:
+          PerformMwCAS();
+          break;
+        default:
+          break;
+      }
+      const auto end_time = std::chrono::high_resolution_clock::now();
+      const auto exec_time =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+      exec_times_nano_.emplace_back(exec_time);
+    }
+  }
+
+  void
+  MeasureThroughput()
+  {
+    assert(operation_queue_.size() == operation_counts_);
+
+    const auto start_time = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < operation_counts_; ++i) {
+      switch (operation_queue_[i]) {
+        case kRead:
+          ReadMwCASField();
+          break;
+        case kWrite:
+          PerformMwCAS();
+          break;
+        default:
+          break;
+      }
+    }
+    const auto end_time = std::chrono::high_resolution_clock::now();
+    exec_time_nano_ =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+  }
+
+  std::pair<const std::vector<Operation> &, const std::vector<size_t> &>
+  GetLatencies() const
+  {
+    return {operation_queue_, exec_times_nano_};
+  }
+
+  size_t
+  GetTotalExecTime() const
+  {
+    return exec_time_nano_;
   }
 };
