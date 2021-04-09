6b728231451ee37394fb54b625d3dc5672735bd9
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-04-09 12:51:47 +0900

    add feature to select MwCAS targets before bench

--- src/worker.hpp
+++ src/worker.hpp
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <random>
 #include <utility>
@@ -23,24 +24,26 @@ class Worker
 
   size_t random_seed_;
 
+  bool seed_is_modified_;
+
   std::vector<Operation> operation_queue_;
 
   std::vector<size_t> exec_times_nano_;
 
   size_t exec_time_nano_;
 
+  size_t shared_field_num_;
+
+  size_t target_field_num_;
+
+  std::vector<std::vector<size_t>> target_fields_;
+
  protected:
   /*################################################################################################
    * Inherited member variables
    *##############################################################################################*/
 
-  size_t shared_num_;
-
-  size_t private_num_;
-
   size_t *shared_fields_;
-
-  size_t private_fields_[kFieldNum];
 
  public:
   /*################################################################################################
@@ -48,24 +51,22 @@ class Worker
    *##############################################################################################*/
 
   Worker(  //
-      const size_t shared_field_num,
-      const size_t private_field_num,
       size_t *shared_fields,
+      const size_t shared_field_num,
+      const size_t target_field_num,
       const size_t read_ratio,
       const size_t operation_counts,
       const size_t random_seed = 0)
       : read_ratio_{read_ratio},
         operation_counts_{operation_counts},
         random_seed_{random_seed},
+        seed_is_modified_{true},
         exec_time_nano_{0},
-        shared_num_{shared_field_num},
-        private_num_{private_field_num}
+        shared_field_num_{shared_field_num},
+        target_field_num_{target_field_num},
+        shared_fields_{shared_fields}
   {
     PrepareBench();
-    shared_fields_ = shared_fields;
-    for (size_t count = 0; count < private_field_num; ++count) {
-      private_fields_[count] = 0;
-    }
   }
 
   ~Worker() = default;
@@ -75,29 +76,50 @@ class Worker
    *##############################################################################################*/
 
   virtual void
-  ReadMwCASField()
+  ReadMwCASField(const size_t index)
   {
   }
 
   virtual void
-  PerformMwCAS()
+  PerformMwCAS(const std::vector<size_t> &target_fields)
   {
   }
 
   void
   PrepareBench()
   {
-    // initialize vectors
+    // initialize execution time
+    exec_time_nano_ = 0;
     exec_times_nano_.clear();
     exec_times_nano_.reserve(operation_counts_);
-    operation_queue_.clear();
-    operation_queue_.reserve(operation_counts_);
 
     // generate an operation-queue for benchmark
-    std::mt19937_64 rand_engine{random_seed_};
-    for (size_t i = 0; i < operation_counts_; ++i) {
-      const auto ops = (rand_engine() % 100 < read_ratio_) ? Operation::kRead : Operation::kWrite;
-      operation_queue_.emplace_back(ops);
+    if (seed_is_modified_) {
+      seed_is_modified_ = false;
+
+      operation_queue_.clear();
+      operation_queue_.reserve(operation_counts_);
+      target_fields_.clear();
+      target_fields_.reserve(operation_counts_);
+
+      std::mt19937_64 rand_engine{random_seed_};
+      for (size_t i = 0; i < operation_counts_; ++i) {
+        // create an operation-queue
+        const auto ops = (rand_engine() % 100 < read_ratio_) ? Operation::kRead : Operation::kWrite;
+        operation_queue_.emplace_back(ops);
+
+        // select target fields for each operation
+        std::vector<size_t> target_field;
+        target_field.reserve(target_field_num_);
+        for (size_t j = 0; j < target_field_num_; ++j) {
+          target_field.emplace_back(rand_engine() % shared_field_num_);
+          if (operation_queue_[i] == Operation::kRead) {
+            break;
+          }
+        }
+        std::sort(target_field.begin(), target_field.end());
+        target_fields_.emplace_back(std::move(target_field));
+      }
     }
   }
 
@@ -105,16 +127,17 @@ class Worker
   MeasureLatency()
   {
     assert(operation_queue_.size() == operation_counts_);
+    assert(target_fields_.size() == operation_counts_);
     assert(exec_times_nano_.empty());
 
     for (size_t i = 0; i < operation_counts_; ++i) {
       const auto start_time = std::chrono::high_resolution_clock::now();
       switch (operation_queue_[i]) {
         case kRead:
-          ReadMwCASField();
+          ReadMwCASField(target_fields_[i][0]);
           break;
         case kWrite:
-          PerformMwCAS();
+          PerformMwCAS(target_fields_[i]);
           break;
         default:
           break;
@@ -130,15 +153,16 @@ class Worker
   MeasureThroughput()
   {
     assert(operation_queue_.size() == operation_counts_);
+    assert(target_fields_.size() == operation_counts_);
 
     const auto start_time = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < operation_counts_; ++i) {
       switch (operation_queue_[i]) {
         case kRead:
-          ReadMwCASField();
+          ReadMwCASField(target_fields_[i][0]);
           break;
         case kWrite:
-          PerformMwCAS();
+          PerformMwCAS(target_fields_[i]);
           break;
         default:
           break;
