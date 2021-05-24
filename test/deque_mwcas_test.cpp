36a7be8a89f3dd30ae1de656b0f030f496aef116
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-05-24 14:48:50 +0900

    add multi-threads tests

--- test/deque_mwcas_test.cpp
+++ test/deque_mwcas_test.cpp
@@ -14,23 +14,40 @@ class DequeMwCASFixture : public ::testing::Test
   using Deque_t = ::dbgroup::container::DequeMwCAS;
 
  public:
-  static constexpr size_t kRepeatCount = 1E5;
+  static constexpr size_t kRepeatNum = 1E5;
+  static constexpr size_t kThreadNum = 8;
 
   Deque_t deque_;
 
   void
-  PushFronts()
+  PushFronts(const size_t repeat_num)
   {
-    for (size_t i = 0; i < kRepeatCount; ++i) {
+    for (size_t i = 0; i < repeat_num; ++i) {
       deque_.PushFront(i);
     }
   }
 
   void
-  PushBacks()
+  PushBacks(const size_t repeat_num)
   {
-    for (size_t i = 0; i < kRepeatCount; ++i) {
+    for (size_t i = 0; i < repeat_num; ++i) {
       deque_.PushBack(i);
+    }
+  }
+
+  void
+  PopFronts(const size_t repeat_num)
+  {
+    for (size_t i = 0; i < repeat_num; ++i) {
+      deque_.PopFront();
+    }
+  }
+
+  void
+  PopBacks(const size_t repeat_num)
+  {
+    for (size_t i = 0; i < repeat_num; ++i) {
+      deque_.PopBack();
     }
   }
 
@@ -57,6 +74,23 @@ TEST_F(DequeMwCASFixture, PushFront_OneItem_DequeIsNotEmpty)
   EXPECT_FALSE(deque_.Empty());
 }
 
+TEST_F(DequeMwCASFixture, PushFront_ByMultiThreads_AllItemsPushed)
+{
+  std::vector<std::thread> threads;
+  for (size_t i = 0; i < kThreadNum; ++i) {
+    threads.emplace_back(&DequeMwCASFixture::PushFronts, this, kRepeatNum);
+  }
+  for (auto &&thread : threads) {
+    thread.join();
+  }
+
+  for (size_t i = 0; i < kRepeatNum * kThreadNum; ++i) {
+    EXPECT_FALSE(deque_.Empty());
+    deque_.PopFront();
+  }
+  EXPECT_TRUE(deque_.Empty());
+}
+
 TEST_F(DequeMwCASFixture, PopFront_AfterPushFront_DequeIsEmpty)
 {
   deque_.PushFront(0);
@@ -71,10 +105,42 @@ TEST_F(DequeMwCASFixture, PopFront_AfterPushBack_DequeIsEmpty)
   EXPECT_TRUE(deque_.Empty());
 }
 
+TEST_F(DequeMwCASFixture, PopFront_ByMultiThreads_AllItemsPopped)
+{
+  PushFronts(kRepeatNum * kThreadNum);
+
+  std::vector<std::thread> threads;
+  for (size_t i = 0; i < kThreadNum; ++i) {
+    threads.emplace_back(&DequeMwCASFixture::PopFronts, this, kRepeatNum);
+  }
+  for (auto &&thread : threads) {
+    thread.join();
+  }
+
+  EXPECT_TRUE(deque_.Empty());
+}
+
 TEST_F(DequeMwCASFixture, PushBack_OneItem_DequeIsNotEmpty)
 {
   deque_.PushBack(0);
   EXPECT_FALSE(deque_.Empty());
+}
+
+TEST_F(DequeMwCASFixture, PushBack_ByMultiThreads_AllItemsPushed)
+{
+  std::vector<std::thread> threads;
+  for (size_t i = 0; i < kThreadNum; ++i) {
+    threads.emplace_back(&DequeMwCASFixture::PushBacks, this, kRepeatNum);
+  }
+  for (auto &&thread : threads) {
+    thread.join();
+  }
+
+  for (size_t i = 0; i < kRepeatNum * kThreadNum; ++i) {
+    EXPECT_FALSE(deque_.Empty());
+    deque_.PopFront();
+  }
+  EXPECT_TRUE(deque_.Empty());
 }
 
 TEST_F(DequeMwCASFixture, PopBack_AfterPushFront_DequeIsEmpty)
@@ -91,9 +157,24 @@ TEST_F(DequeMwCASFixture, PopBack_AfterPushBack_DequeIsEmpty)
   EXPECT_TRUE(deque_.Empty());
 }
 
+TEST_F(DequeMwCASFixture, PopBack_ByMultiThreads_AllItemsPopped)
+{
+  PushFronts(kRepeatNum * kThreadNum);
+
+  std::vector<std::thread> threads;
+  for (size_t i = 0; i < kThreadNum; ++i) {
+    threads.emplace_back(&DequeMwCASFixture::PopBacks, this, kRepeatNum);
+  }
+  for (auto &&thread : threads) {
+    thread.join();
+  }
+
+  EXPECT_TRUE(deque_.Empty());
+}
+
 TEST_F(DequeMwCASFixture, Front_AfterPushFronts_ReadPushedItems)
 {
-  for (size_t i = 0; i < kRepeatCount; ++i) {
+  for (size_t i = 0; i < kRepeatNum; ++i) {
     deque_.PushFront(i);
     EXPECT_EQ(deque_.Front(), i);
   }
@@ -101,8 +182,8 @@ TEST_F(DequeMwCASFixture, Front_AfterPushFronts_ReadPushedItems)
 
 TEST_F(DequeMwCASFixture, Front_AfterPushBacks_ReadPushedItems)
 {
-  PushBacks();
-  for (size_t i = 0; i < kRepeatCount; ++i) {
+  PushBacks(kRepeatNum);
+  for (size_t i = 0; i < kRepeatNum; ++i) {
     EXPECT_EQ(deque_.Front(), i);
     deque_.PopFront();
   }
@@ -110,7 +191,7 @@ TEST_F(DequeMwCASFixture, Front_AfterPushBacks_ReadPushedItems)
 
 TEST_F(DequeMwCASFixture, Back_AfterPushBacks_ReadPushedItems)
 {
-  for (size_t i = 0; i < kRepeatCount; ++i) {
+  for (size_t i = 0; i < kRepeatNum; ++i) {
     deque_.PushBack(i);
     EXPECT_EQ(deque_.Back(), i);
   }
@@ -118,8 +199,8 @@ TEST_F(DequeMwCASFixture, Back_AfterPushBacks_ReadPushedItems)
 
 TEST_F(DequeMwCASFixture, Back_AfterPushFronts_ReadPushedItems)
 {
-  PushFronts();
-  for (size_t i = 0; i < kRepeatCount; ++i) {
+  PushFronts(kRepeatNum);
+  for (size_t i = 0; i < kRepeatNum; ++i) {
     EXPECT_EQ(deque_.Back(), i);
     deque_.PopBack();
   }
