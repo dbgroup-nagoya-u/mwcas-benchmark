90e92251d6f7a852fef845c6a9bf5b6e167c2673
dhingarkan <unnomotoki@gmail.com>
2026-01-26 13:26:52 +0900

    update mwcas_bench.cpp: output version wrap counter

--- src/mwcas_bench.cpp
+++ src/mwcas_bench.cpp
@@ -179,7 +179,12 @@ main(  //
   if (FLAGS_dlf_mwcas) RunBenchmark<DLFMwCAS>("Deadlock-free MwCAS", target_num);
   if (FLAGS_casn) RunBenchmark<CASN>("CASN", target_num);
   if (FLAGS_aopt) RunBenchmark<AOPT>("AOPT", target_num);
-  if (FLAGS_lf_mwcas) RunBenchmark<LFMwCAS>("Lock-free MwCAS", target_num);
+  if (FLAGS_lf_mwcas) {
+    RunBenchmark<LFMwCAS>("Lock-free MwCAS", target_num);
+    std::cout << dbgroup::atomic::mwcas::lock_free::MwCASDescriptor::
+            GetVersionWrapCountSum()  // Total Version Wrap Count
+              << std::endl;
+  }
 #ifdef MWCAS_BENCH_USE_PMWCAS
   if (FLAGS_pmwcas) RunBenchmark<PMwCAS>("PMwCAS", target_num);
 #endif
